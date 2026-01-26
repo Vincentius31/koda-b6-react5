2288f0fe2591751ddacbe939e850bdcaf9b54450
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-01-27 01:03:45 +0700

    feat: adding route using react-router-dom to change the page from survey to results

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FormPage from "./Pages/FormPage";
+import ResultPage from "./Pages/ResultPage";
+import Navbar from "./Components/Navbar";
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<FormPage/>}/>
+        <Route path="/results" element={<ResultPage/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
